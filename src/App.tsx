f4da544e46dc72c4799ce22a3a575451bbd06b7d
Vadim Kiniabaev <thekinv21@gmail.com>
2024-07-15 14:02:53 +0300

    FEAT: [master-xxx][vadim]: added nginx conf and logo nginx & ubuntu for app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
-import { useState } from 'react'
 import './App.scss'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import nginxLogo from '/nginx.svg'
+import ubuntuLogo from '/ubuntu.svg'
 
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<>
 			<div>
 				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
+					<img src={nginxLogo} className='logo' alt='Vite logo' />
 				</a>
 				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
+					<img src={ubuntuLogo} className='logo react' alt='React logo' />
 				</a>
 			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
+			<h1>Deployment Vps Ubuntu 20.04 + Nginx</h1>
 			<p className='read-the-docs'>
-				Create project for deploying with VPS and NGINX
+				Vite project for deploying with VPS and NGINX
 			</p>
 		</>
 	)
